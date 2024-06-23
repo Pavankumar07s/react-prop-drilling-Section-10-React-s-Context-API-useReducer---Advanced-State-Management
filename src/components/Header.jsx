7e60c4f4d4b1e6b181d62c109a727a3a0957a57e
Pavankumar07s <pawankumar14662693@gmail.com>
2024-06-23 12:00:10 +0530

    shifted project to use context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { useRef } from 'react';
-
+import { useContext } from 'react';
 import CartModal from './CartModal.jsx';
+import { CartContext } from '../store/Shoping-cart-context.jsx';
+export default function Header() {
+  const {items}=useContext(CartContext);
+  const {UpdateCartItemQuantity}=useContext(CartContext);
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
   const modal = useRef();
 
-  const cartQuantity = cart.items.length;
+  const cartQuantity = items.length;
 
   function handleOpenCartClick() {
     modal.current.open();
@@ -22,12 +25,13 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
     );
   }
 
+ 
   return (
     <>
       <CartModal
         ref={modal}
-        cartItems={cart.items}
-        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
+        cartItems={items}
+        onUpdateCartItemQuantity={UpdateCartItemQuantity}
         title="Your Cart"
         actions={modalActions}
       />
